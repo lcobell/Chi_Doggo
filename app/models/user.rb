ef4ab1c93cb1698dec09a43bc843e1bcd700e8b1
Logan CoBell <lcobell@cps.edu>
2022-12-06 17:43:20 +0000

    edited some associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,9 +43,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :dogs, class_name: "Dog", foreign_key: "owner_id"
+         has_many :dogs, foreign_key: "owner_id"
 
-         has_many :walks, class_name: "Walk", foreign_key: "walker_id"
+         has_many :walks, foreign_key: "walker_id"
 
          has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
 
