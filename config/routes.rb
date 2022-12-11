aeeb71528e6de5a975c60f010d2c485bc72098d3
Logan CoBell <lcobell@cps.edu>
2022-12-11 20:55:11 +0000

    added bootstrap navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'dogs#index'
+  devise_for :users
+  resources :dogs
   resources :messages
   resources :walks
-  resources :dogs
-  devise_for :users
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'dogs#index'
+  
 end
