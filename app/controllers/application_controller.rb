847831871daffc73e812c6d52e0b31cb367e31db
Logan CoBell <lcobell@cps.edu>
2022-12-12 01:05:38 +0000

    Added aler and flash notices and spaced navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username)}
   end
