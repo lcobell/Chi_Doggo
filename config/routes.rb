c6e854fb8785793360d8ce1cdf33f9947ded5ada
Logan CoBell <lcobell@cps.edu>
2022-12-12 18:29:21 +0000

    working on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'dogs#index'
 
-  get "/users/:id" => "users#show", as: :user
+ 
 
   
 
   devise_for :users
   resources :dogs
   resources :messages
-  resources :users, only: [:show]
+  resources :users, only: :show
   resources :walks
   
+  # get "/users/:id" => "users#show", as: :user
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
